793ccc1cb071d6c85f24bf47357b3020c36f5cc6
Isaac Pinheiro <isaacpnhr@gmail.com>
2016-07-13 01:04:31 -0300

    Finalizing the core file.

--- src/word_frequency/core.clj
+++ src/word_frequency/core.clj
@@ -3,4 +3,25 @@
     (:gen-class))
 
 (defn -main [& args]
-    (println "Hello, World!"))
+
+    (println)
+    (println "########## Word Frequency ##########")
+    (println)
+    (println "Please, inform some text: ")
+    (println)
+    (def text (read-line))
+    (println)
+
+    (def wordFrequency
+        (parser/transform
+            (parser/group
+                (parser/toList text))))
+
+    (loop [v wordFrequency]
+        (if (not= v [])
+            (do
+                (def wf (first v))
+                (def res (str "Word: " (wf :word) ", Frequency: " (wf :frequency)))
+                (println res)
+                (recur (rest v))))))
+
